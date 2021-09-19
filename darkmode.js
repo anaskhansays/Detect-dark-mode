df44d7152e0cb0aa7495ecc2391b221fca5db95b
BornKhan <anas75313@gmail.com>
2021-09-19 23:58:49 +0530

    "Intital commit"

---
+++ darkmode.js
@@ -0,0 +1,21 @@
+const btn = document.getElementById("btn-toggle");
+const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
+
+btn.addEventListener("click", function () {
+  if (prefersDarkScheme.matches) {
+    document.body.classList.toggle("light-theme");
+  } else {
+    document.body.classList.toggle("dark-theme");
+  }
+});
+
+
+
+
+
+
+
+
+
+
+
